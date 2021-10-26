5d6e733f9540e9a51b64d6a7dd5f08077f354ea5
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-10-26 13:45:59 +0100

    All tests completed

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -2,6 +2,24 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
+        self.passengers = []
 
     def drive(self):
-        print("Brum, brum")
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, passenger):
+        self.passengers.remove(passenger)
+
+    def empty(self):
+        self.passengers = []
+
+    def pick_up_from_stop(self, stop):
+        for person in stop.queue:
+            self.pick_up(stop.queue)
+        stop.queue.clear()
--- bus_lab_start_code/src/bus_stop.py
+++ bus_lab_start_code/src/bus_stop.py
@@ -2,3 +2,12 @@ class BusStop:
     def __init__(self, name):
         self.queue = []
         self.name = name
+
+    def queue_length(self):
+        return len(self.queue)
+
+    def add_to_queue(self, person):
+        self.queue.append(person)
+
+    def clear(self):
+        self.queue.clear()
